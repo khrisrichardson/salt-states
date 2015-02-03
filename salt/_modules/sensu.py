6d67dfaf57be0ce39e240aa9710e4770acc5688f
Khris Richardson <khris.richardson@gmail.com>
2015-02-02 18:46:16 -0800

    setup service before check vs. concurrently

--- salt/_modules/sensu.py
+++ salt/_modules/sensu.py
@@ -43,7 +43,7 @@ def list_checks(role=None):
         if role in _list_roles() \
         and not __salt__['file.file_exists'](path):
             __salt__['state.sls'](role)
-            __salt__['state.sls'](role + '.relate-sensu-api'),
+            __salt__['state.sls'](role + '.relate-sensu-api',
                                   pillar={'test': True})
         ret += _list_values(key='checks', pattern='checks-' + role)
     return ret
